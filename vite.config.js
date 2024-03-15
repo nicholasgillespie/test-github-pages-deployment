d618378a2204b03a0ea439bd81f38c7bed9a085d
Nicholas Gillespie <contact@nicholasgillespie.com>
2024-03-15 11:35:14 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/test-github-pages-deployment/",
+  // server: {
+  //   port: 3000,
+  // },
+  // build: {
+  //   outDir: "dist",
+  // },
+});
